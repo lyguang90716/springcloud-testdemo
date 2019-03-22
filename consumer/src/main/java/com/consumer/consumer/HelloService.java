759ec02d53d71b404b7ff2976edaef7c0a76ba07
git <l_y_g90716@hotmail.com>
2019-03-22 08:59:26 +0800

    first commit ,git test

--- consumer/src/main/java/com/consumer/consumer/HelloService.java
+++ consumer/src/main/java/com/consumer/consumer/HelloService.java
@@ -14,7 +14,7 @@ public class HelloService {
     @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name)
     {
-        return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
+        return restTemplate.getForObject("http://API-GATEWAY-ZUUL/api-hi/hi/" + name, String.class);
     }
 
     public String hiError(String name)
--- product/src/main/java/com/product/product/ProductApplication.java
+++ product/src/main/java/com/product/product/ProductApplication.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -20,8 +20,8 @@ public class ProductApplication {
     @Value("${server.port}")
     String port;
 
-    @RequestMapping("/hi")
-    public String home(@RequestParam String name){
+    @RequestMapping(value = "/hi/{name}")
+    public String home(@PathVariable String name){
         return "hi " + name + ",i am from port:" + port;
     }
 
