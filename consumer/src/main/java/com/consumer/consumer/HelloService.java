869f1db340b141a693959aa26ea014ea1589792d
git <l_y_g90716@hotmail.com>
2019-03-25 16:36:21 +0800

    config server , config syn demo

--- consumer/src/main/java/com/consumer/consumer/HelloService.java
+++ consumer/src/main/java/com/consumer/consumer/HelloService.java
@@ -11,15 +11,15 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+
     @HystrixCommand(fallbackMethod = "hiError")
-    public String hiService(String name)
-    {
+    public String hiService(String name) {
         return restTemplate.getForObject("http://API-GATEWAY-ZUUL/api-hi/hi/" + name, String.class);
     }
 
-    public String hiError(String name)
-    {
+    public String hiError(String name) {
         return "hey " +
                 name + ", there is some problem with hi page";
     }
+
 }
--- product/src/main/java/com/product/product/ProductApplication.java
+++ product/src/main/java/com/product/product/ProductApplication.java
@@ -3,6 +3,8 @@ package com.product.product;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableDiscoveryClient
+@RefreshScope
 public class ProductApplication {
 
     public static void main(String[] args) {
@@ -20,9 +24,12 @@ public class ProductApplication {
     @Value("${server.port}")
     String port;
 
+    @Value("${test}")
+    String test;
+
     @RequestMapping(value = "/hi/{name}")
     public String home(@PathVariable String name){
-        return "hi " + name + ",i am from port:" + port;
+        return "hi " + name + ",i am from port:"  + port + ",config info:"  + test;
     }
 
 }
